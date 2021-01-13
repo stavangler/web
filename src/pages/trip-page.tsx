41cd8be871de2ab2d933bc1935b8edd07f8ef8de
nniclas <niclas.oes@gmail.com>
2021-01-13 14:24:46 +0100

    tab menu

--- src/pages/trip-page.tsx
+++ src/pages/trip-page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import { Box, Tab, Tabs, Typography } from '@material-ui/core'
+import TabMenu from '../components/tab-menu'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -14,88 +15,17 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 )
 
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    )
-}
-
-function a11yProps(index: any) {
-    return {
-        id: `vertical-tab-${index}`,
-        'aria-controls': `vertical-tabpanel-${index}`,
-    }
-}
-
-
-
 
 const TripPage = () => {
     const classes = useStyles()
-    const [value, setValue] = React.useState(0)
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-        setValue(newValue)
-    }
 
     return (
         <div className={classes.root}>
-            <Tabs
-                orientation="vertical"
-                variant="scrollable"
-                value={value}
-                onChange={handleChange}
-                aria-label="Vertical tabs example"
-                className={classes.tabs}
-            >
-                <Tab label="Item One" {...a11yProps(0)} />
-                <Tab label="Item Two" {...a11yProps(1)} />
-                <Tab label="Item Three" {...a11yProps(2)} />
-                <Tab label="Item Four" {...a11yProps(3)} />
-                <Tab label="Item Five" {...a11yProps(4)} />
-                <Tab label="Item Six" {...a11yProps(5)} />
-                <Tab label="Item Seven" {...a11yProps(6)} />
-            </Tabs>
-            <TabPanel value={value} index={0}>
-                Item One
-                </TabPanel>
-            <TabPanel value={value} index={1}>
-                Item Two
-                </TabPanel>
-            <TabPanel value={value} index={2}>
-                Item Three
-                </TabPanel>
-            <TabPanel value={value} index={3}>
-                Item Four
-                </TabPanel>
-            <TabPanel value={value} index={4}>
-                Item Five
-                </TabPanel>
-            <TabPanel value={value} index={5}>
-                Item Six
-                </TabPanel>
-            <TabPanel value={value} index={6}>
-                Item Seven
-                </TabPanel>
+            <TabMenu children={[
+                { label: 'Information', content: <Box m={2}>Info page todo</Box> },
+                { label: 'Agenda', content: <Box m={1}>Agenda page todo</Box> },
+                { label: 'Participants', content: <Box m={3}>Participants todo</Box> }
+            ]}></TabMenu>
         </div>
     )
 }
